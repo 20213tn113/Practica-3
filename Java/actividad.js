f5a8046aff3579eb09623470da26fa59b91a6dec
AlanGlezz <20213tn113@utez.edu.mx>
2022-06-05 14:25:51 -0500

    Practica 3

---
+++ Java/actividad.js
@@ -0,0 +1,20 @@
+
+document.write("<img src='/Imagenes/silueta.png' id='img1' >");
+
+document.write("<table >");
+document.write("<tr>");
+document.write("<td id='td1'>Nombre: </td>");
+document.write("<td>Alan Javier González Pineda</td>");
+document.write("</tr>");
+document.write("<td id='td1'>Edad: </td>");
+document.write("<td>19 años</td>");
+document.write("</tr>");
+document.write("<td id='td1'>Localidad: </td>");
+document.write("<td>Morelos</td>");
+document.write("</tr>");
+document.write("<td id='td1'>Profesión: </td>");
+document.write("<td>Estudiante</td>");
+document.write("</tr>");
+
+document.write("</tr>");
+document.write("</table>");
